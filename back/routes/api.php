1677d4ce3b42a39ae2fd9a0919c372612e126f17
Kārlis <voiconokskarls@gmail.com>
2025-11-18 22:13:52 +0200

    fix auth sanctum

--- back/routes/api.php
+++ back/routes/api.php
@@ -9,7 +9,7 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::post('/store-url', [UrlController::class, 'store']);
+Route::post('/store-url', [UrlController::class, 'store'])->middleware('auth:sanctum');
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
