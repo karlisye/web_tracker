900077aa3983b94639a471c39c5f068cb76edc54
Kārlis <voiconokskarls@gmail.com>
2025-12-22 16:23:01 +0200

    add functional title

--- extension/src/popup/layouts/Head.jsx
+++ extension/src/popup/layouts/Head.jsx
@@ -1,7 +1,26 @@
 function Head() {
+
+  const redirectToLogin = () => {
+    if (window.chrome && chrome.runtime) {
+      chrome.runtime.sendMessage(
+        { 
+          type: 'redirect-to-login',
+        },
+        (response) => {
+          console.log("Message sent to Chrome extension:", response);
+        }
+      );
+    }
+  }
+  
   return (
     <div className="text-center p-2 bg-indigo-300 text-white rounded-md">
-        <h1 className="text-4xl font-bold">WEB TRACKER</h1>
+        <h1 
+          className="text-4xl font-bold hover:cursor-pointer"
+          onClick={redirectToLogin}
+        >
+          WEB TRACKER
+        </h1>
     </div>
   )
 }
