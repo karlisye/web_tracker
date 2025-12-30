f998558e50122894ba0e569c777e6455cdb92bb0
Kārlis <voiconokskarls@gmail.com>
2025-12-30 14:05:45 +0200

    improve visitcount display width

--- web/src/components/MostVisitsGraph.jsx
+++ web/src/components/MostVisitsGraph.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
-import { BarChart, Bar, XAxis, YAxis, Tooltip, CartesianGrid } from "recharts";
+import { BarChart, Bar, XAxis, YAxis, Tooltip, CartesianGrid, ResponsiveContainer } from "recharts";
 
 const MostVisitsGraph = () => {
   const [mostVisits, setMostVisits] = useState(null);
@@ -19,13 +19,15 @@ const MostVisitsGraph = () => {
         <div>
         <h2 className='text-2xl my-2 font-bold ml-2'>Visit Count</h2>
         <div className='bg-white rounded-2xl shadow-lg hover:shadow-xl transition-shadow duration-300 p-8 md:p-10'>
-          <BarChart width={500} height={300} data={mostVisits}>
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="host" />
-            <YAxis />
-            <Tooltip />
-            <Bar dataKey="total" fill="#8884d8" />
-          </BarChart>
+          <ResponsiveContainer width='100%' height={300}>
+            <BarChart data={mostVisits}>
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="host" />
+              <YAxis />
+              <Tooltip />
+              <Bar dataKey="total" fill="#8884d8" />
+            </BarChart>
+          </ResponsiveContainer>
         </div>
         </div>
       )}
