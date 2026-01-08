f158b7972a60068ca322b09ee26b7beb8b20cd13
Kārlis <voiconokskarls@gmail.com>
2026-01-08 20:10:03 +0200

    add final changes to Menu

--- web/src/components/Menu.jsx
+++ web/src/components/Menu.jsx
@@ -2,15 +2,27 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 
 const Menu = ({ setIsMenuClicked }) => {
+  let timeout;
   return (
-    <div className='bg-indigo-700/50 backdrop-blur-xs rounded-xl absolute w-60 flex flex-col gap-2 pt-8 px-1 pb-2 text-white z-1'>
-      <button onClick={() => setIsMenuClicked(false)} className='absolute right-0 top-0 mx-2 hover:cursor-pointer hover:scale-105'>
-        <span className='text-xl font-bold'>x</span>
-      </button>
+    <div 
+      className='bg-indigo-700/50 backdrop-blur-xs rounded-xl flex gap-2 py-2 px-1 text-white z-1 flex-1'
+      onMouseLeave={() => {
+        timeout = setTimeout(() => setIsMenuClicked(false), 300);
+      }}
+      onMouseEnter={() => clearTimeout(timeout)}
+    >
+      <div className='flex flex-col flex-1'>
+        <NavLink className={({ isActive }) => `${isActive && 'bg-indigo-500/50 backdrop-blur-2xl border-l-6 border-white/50 shadow-md'} p-2 rounded-md font-semibold w-full`} to='/'>Home</NavLink>
+        <NavLink className={({ isActive }) => `${isActive && 'bg-indigo-500/50 backdrop-blur-2xl border-l-6 border-white/50 shadow-md'} p-2 rounded-md font-semibold w-full`} to='/dashboard'>Dashboard</NavLink>
+        <NavLink className={({ isActive }) => `${isActive && 'bg-indigo-500/50 backdrop-blur-2xl border-l-6 border-white/50 shadow-md'} p-2 rounded-md font-semibold w-full`} to='/about'>About Us</NavLink>
+      </div>
 
-      <NavLink className={({ isActive }) => `${isActive && 'bg-indigo-500/50 backdrop-blur-2xl border-l-6 border-white/50 shadow-md'} p-2 rounded-md font-semibold w-full`} to='/'>Home</NavLink>
-      <NavLink className={({ isActive }) => `${isActive && 'bg-indigo-500/50 backdrop-blur-2xl border-l-6 border-white/50 shadow-md'} p-2 rounded-md font-semibold w-full`} to='/dashboard'>Dashboard</NavLink>
-      <NavLink className={({ isActive }) => `${isActive && 'bg-indigo-500/50 backdrop-blur-2xl border-l-6 border-white/50 shadow-md'} p-2 rounded-md font-semibold w-full`} to='/about'>About Us</NavLink>
+      <button 
+        className='text-2xl font-bold mr-2 hover:scale-110 hover:cursor-pointer'
+        onClick={() => setIsMenuClicked(false)}
+      >
+        x
+      </button>
     </div>
   )
 }
