9f1110fed1c114bd3e3a39b6313d15f5c7535840
Kārlis <voiconokskarls@gmail.com>
2026-01-18 14:12:10 +0200

    add little improvements to dashboard styling

--- web/src/components/ContainerTitle.jsx
+++ web/src/components/ContainerTitle.jsx
@@ -6,7 +6,7 @@ const ContainerTitle = ({ title, text }) => {
       <h2 className='text-2xl font-bold'>{title}</h2>
 
       {text && (
-        <div className='group relative'>
+        <div className='group relative z-10'>
           <span className='flex items-center justify-center w-5 h-5 rounded-full border text-sm text-slate-600'>?</span>
           <span className='absolute hidden group-hover:block bg-slate-900 text-white text-xs whitespace-nowrap p-1 rounded-md'>{text}</span>
         </div>
--- web/src/components/navbar/DashboardNavigation.jsx
+++ web/src/components/navbar/DashboardNavigation.jsx
@@ -9,28 +9,28 @@ const DashboardNavigation = ({ onNavigate }) => {
       <div className='bg-white rounded-2xl shadow-lg hover:shadow-xl transition-shadow duration-300 p-8'>
         <div className='flex gap-2 justify-between text-slate-800 font-bold items-center my-1 h-full'>
           <button
-            className='bg-indigo-200 py-5 rounded-md grow shadow-md hover:bg-indigo-300 hover:shadow-lg hover:cursor-pointer'
+            className='bg-indigo-200 py-5 rounded-md grow shadow-md hover:bg-indigo-300 hover:shadow-lg hover:cursor-pointer transition duration-200'
             onClick={() => onNavigate('visits')}
           >
             Login history
           </button>
 
           <button
-            className='bg-indigo-200 py-5 rounded-md grow shadow-md hover:bg-indigo-300 hover:shadow-lg hover:cursor-pointer'
+            className='bg-indigo-200 py-5 rounded-md grow shadow-md hover:bg-indigo-300 hover:shadow-lg hover:cursor-pointer transition duration-200'
             onClick={() => onNavigate('mostVisits')}
           >
             Visit count
           </button>
 
           <button
-            className='bg-indigo-200 py-5 rounded-md grow shadow-md hover:bg-indigo-300 hover:shadow-lg hover:cursor-pointer'
+            className='bg-indigo-200 py-5 rounded-md grow shadow-md hover:bg-indigo-300 hover:shadow-lg hover:cursor-pointer transition duration-200'
             onClick={() => onNavigate('inactive')}
           >
             Inactive websites
           </button>
 
           <button
-            className='bg-indigo-200 py-5 rounded-md grow shadow-md hover:bg-indigo-300 hover:shadow-lg hover:cursor-pointer'
+            className='bg-indigo-200 py-5 rounded-md grow shadow-md hover:bg-indigo-300 hover:shadow-lg hover:cursor-pointer transition duration-200'
             onClick={() => onNavigate('websites')}
           >
             Your websites
