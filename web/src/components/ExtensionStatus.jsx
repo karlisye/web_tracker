72763fd2470b6fbeb7dd498c65dd60032cb040f1
Kārlis <voiconokskarls@gmail.com>
2026-01-19 15:34:53 +0200

    add display changes to extension status

--- web/src/components/ExtensionStatus.jsx
+++ web/src/components/ExtensionStatus.jsx
@@ -9,7 +9,8 @@ const ExtensionStatus = () => {
   const { user } = useContext(AppContext);
 
   const [isLinked, setIsLinked] = useState(false);
-  const [isHovered, setIsHovered] = useState(false);
+  const [isHovered, setIsHovered] = useState(true);
+  const [isHidden,  setIsHidden] = useState(false);
 
   const isExtensionLinked = () => {
     if (window.chrome && chrome.runtime) {
@@ -26,43 +27,66 @@ const ExtensionStatus = () => {
   },[user]);
 
   return (
-    <div 
-      className={`fixed bottom-0 right-0 p-2 flex flex-col gap-2 text-lg rounded-lg m-2 text-white bg-linear-to-br
-        ${isLinked ? 'from-green-400 to-green-600' : 'from-red-400 to-red-600'}
-      `}
+    <div className='fixed bottom-0 right-0 flex m-2 items-center z-10'
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
-      <div className='flex gap-2'>
-        <span>Extension status: <span className='font-bold'>{isLinked ? 'linked' : 'unlinked'}</span></span>
-
-        <svg className='w-5 hover:cursor-pointer' onClick={isExtensionLinked} fill="rgb(255,255,255)" viewBox="0 0 24.00 24.00" xmlns="http://www.w3.org/2000/svg" stroke="#000000" strokeWidth="0.00024000000000000003">
-          <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
-          <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
-          <g id="SVGRepo_iconCarrier"> 
-            <path d="M19.146 4.854l-1.489 1.489A8 8 0 1 0 12 20a8.094 8.094 0 0 0 7.371-4.886 1 1 0 1 0-1.842-.779A6.071 6.071 0 0 1 12 18a6 6 0 1 1 4.243-10.243l-1.39 1.39a.5.5 0 0 0 .354.854H19.5A.5.5 0 0 0 20 9.5V5.207a.5.5 0 0 0-.854-.353z"></path>
-          </g>
-        </svg>
-      </div>
-
-      {isHovered && (
-        <div className='flex justify-center'>
-          <button 
-            className={`border-2 rounded-md py-1 px-3 hover:cursor-pointer hover:scale-105 font-semibold
-              ${isLinked ? 'bg-green-600 hover:bg-green-700' : 'bg-red-600 hover:bg-red-700'}
-            `}
-            onClick={isLinked ? () => {
-              unlink();
-              setIsLinked(false);
-            } : () => { 
-              link(user.extension_token);
-              setIsLinked(true);
-            }}
+          <button className={`h-6 hover:cursor-pointer hover:scale-105
+            ${isHidden ? 'w-6' : 'max-w-0'}`}
+            onClick={() => setIsHidden(prev => !prev)}
           >
-            {isLinked ? 'Unlink' : 'Link'}
+            <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path fillRule="evenodd" clipRule="evenodd" d="M15.7071 5.29289C16.0976 5.68342 16.0976 6.31658 15.7071 6.70711L10.4142 12L15.7071 17.2929C16.0976 17.6834 16.0976 18.3166 15.7071 18.7071C15.3165 19.0976 14.6834 19.0976 14.2929 18.7071L8.46963 12.8839C7.98148 12.3957 7.98148 11.6043 8.46963 11.1161L14.2929 5.29289C14.6834 4.90237 15.3165 4.90237 15.7071 5.29289Z" fill="currentColor"></path> </g></svg>
           </button>
-        </div>
-      )}
+
+          <div className={`flex
+            ${isHidden ? 'w-0 h-20' : ''}`}
+          >
+            <button className={`ml-auto hover:cursor-pointer rounded-l-full bg-red-600 text-white
+              ${isHovered ? 'w-5' : 'max-w-0'}`}
+              onClick={() => setIsHidden(prev => !prev)}
+            >
+              <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path fillRule="evenodd" clipRule="evenodd" d="M8.29289 5.29289C8.68342 4.90237 9.31658 4.90237 9.70711 5.29289L15.5303 11.1161C16.0185 11.6043 16.0185 12.3957 15.5303 12.8839L9.70711 18.7071C9.31658 19.0976 8.68342 19.0976 8.29289 18.7071C7.90237 18.3166 7.90237 17.6834 8.29289 17.2929L13.5858 12L8.29289 6.70711C7.90237 6.31658 7.90237 5.68342 8.29289 5.29289Z" fill="currentColor"></path> </g></svg>      
+            </button>
+
+            <div 
+              className={`p-2 flex flex-col text-lg text-white shadow-lg transition-all duration-300 items-center
+                ${isLinked ? 'bg-green-500' : 'bg-red-500'}
+                ${isHovered ? 'rounded-r-lg' : 'rounded-lg'}
+              `}
+            >
+              <div className='flex gap-2'>
+                <span>Extension status: <span className='font-bold'>{isLinked ? 'linked' : 'unlinked'}</span></span>
+
+                <svg className='w-5 hover:cursor-pointer' onClick={isExtensionLinked} fill="rgb(255,255,255)" viewBox="0 0 24.00 24.00" xmlns="http://www.w3.org/2000/svg" stroke="#000000" strokeWidth="0.00024000000000000003">
+                  <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                  <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
+                  <g id="SVGRepo_iconCarrier"> 
+                    <path d="M19.146 4.854l-1.489 1.489A8 8 0 1 0 12 20a8.094 8.094 0 0 0 7.371-4.886 1 1 0 1 0-1.842-.779A6.071 6.071 0 0 1 12 18a6 6 0 1 1 4.243-10.243l-1.39 1.39a.5.5 0 0 0 .354.854H19.5A.5.5 0 0 0 20 9.5V5.207a.5.5 0 0 0-.854-.353z"></path>
+                  </g>
+                </svg>
+              </div>
+
+              <div className={`overflow-hidden transition-all duration-300 ${isHovered ? 'max-h-20 opacity-100' : 'max-h-0 opacity-0'}`}>
+                <div className='flex justify-center mt-2'>
+                  <button 
+                    className={`rounded-md py-1 px-5 hover:cursor-pointer font-semibold shadow-md transition
+                      ${isLinked ? 'bg-green-600 hover:bg-green-700' : 'bg-red-600 hover:bg-red-700'}
+                    `}
+                    onClick={isLinked ? () => {
+                      unlink();
+                      setIsLinked(false);
+                    } : () => { 
+                      link(user.extension_token);
+                      setIsLinked(true);
+                    }}
+                  >
+                    {isLinked ? 'Unlink' : 'Link'}
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+
     </div>
   )
 }
