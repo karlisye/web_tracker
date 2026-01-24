f73d59268b404dfe30838c171f674abc31c2b2af
Kārlis <voiconokskarls@gmail.com>
2026-01-24 18:43:20 +0200

    add coloring to modals

--- web/src/components/modals/Modal.jsx
+++ web/src/components/modals/Modal.jsx
@@ -6,7 +6,7 @@ const Modal = ({ setIsActive, isActive, children }) => {
 
   return (
     <div onClick={() => setIsActive(false)} className='fixed w-screen h-screen top-0 left-0 flex justify-center items-center backdrop-blur-sm z-1'>
-      <div className='p-4 bg-slate-200 w-2/3 h-2/3 rounded-2xl flex flex-col gap-4'>
+      <div className='p-4 bg-neutral-800 w-2/3 rounded-2xl flex flex-col gap-4'>
         {children}
       </div>
     </div>
--- web/src/components/modals/WebsiteInfo.jsx
+++ web/src/components/modals/WebsiteInfo.jsx
@@ -23,20 +23,20 @@ const WebsiteInfo = ({ websiteID }) => {
         <div>
           <ContainerTitle title={`${visits[0]?.website?.host} visits`} />
 
-          <div className='bg-white rounded-2xl shadow-lg hover:shadow-xl transition-shadow duration-300 p-4 overflow-scroll max-h-50'>
+          <div className='bg-teal-700 rounded-2xl shadow-lg hover:shadow-xl transition-shadow duration-300 p-4 overflow-scroll max-h-50'>
             <table className='w-full table-fixed text-left border-separate border-spacing-y-2 text-slate-600'>
               <thead>
                 <tr className='shadow-sm shadow-slate-300 rounded-md'>
-                  <th className='bg-slate-100 p-2 rounded-l-md'>website</th>
-                  <th className='bg-slate-100 p-2 rounded-r-md'>time</th>
+                  <th className='bg-yellow-100 p-2 rounded-l-md'>website</th>
+                  <th className='bg-yellow-100 p-2 rounded-r-md'>time</th>
                 </tr>
               </thead>
 
               <tbody>
                 {visits.map(visit => (
                   <tr key={visit.id} className='shadow-sm shadow-slate-300 rounded-md'>
-                    <td className='bg-slate-100 p-2 rounded-l-md'>{visit.website.host}</td>
-                    <td className='bg-slate-100 p-2 rounded-r-md'>{visit.visit_time}</td>
+                    <td className='bg-yellow-100 p-2 rounded-l-md'>{visit.website.host}</td>
+                    <td className='bg-yellow-100 p-2 rounded-r-md'>{visit.visit_time}</td>
                   </tr>
                 ))}
               </tbody>
@@ -44,16 +44,18 @@ const WebsiteInfo = ({ websiteID }) => {
           </div>
         </div>
 
-        <div className='bg-white rounded-2xl shadow-lg hover:shadow-xl transition-shadow duration-300 max-h-1/3 grow p-2 flex gap-4'>
-          <div className='flex-1 bg-white shadow-lg rounded-xl h-full w-full hover:shadow-xl transition-shadow duration-300 p-4 flex flex-col justify-center gap-1'>
-            <span className='text-slate-600'>Last time visited: <span className='text-indigo-400 font-semibold'>{visits[0]?.visit_time}</span></span>
-            <span className='text-slate-600'>First time visited: <span className='text-indigo-400 font-semibold'>{visits[visits.length - 1]?.visit_time}</span></span>
+        <div className='bg-teal-700 rounded-2xl shadow-lg hover:shadow-xl transition-shadow duration-300 max-h-1/3 grow p-2 flex gap-4'>
+          <div className='flex-1 bg-yellow-100 shadow-lg rounded-xl hover:shadow-xl transition-shadow duration-300 p-4 flex flex-col justify-center gap-1'>
+            <span className='text-slate-600'>Last time visited: <span className='text-teal-700 font-semibold'>{visits[0]?.visit_time}</span></span>
+            <span className='text-slate-600'>First time visited: <span className='text-teal-700 font-semibold'>{visits[visits.length - 1]?.visit_time}</span></span>
           </div>
-          <div className='flex-1 bg-white shadow-lg rounded-xl h-full w-full hover:shadow-xl transition-shadow duration-300 p-4 flex flex-col justify-center gap-1'>
-            <a target='_blank' href={visits[0]?.website.page_url} className='bg-indigo-100 py-4 shadow-sm hover:shadow-md hover:cursor-pointer rounded-md text-slate-600 text-center'>Go to website</a>
+
+          <div className='flex-1 bg-yellow-100 shadow-lg rounded-xl hover:shadow-xl transition-shadow duration-300 p-4 flex flex-col justify-center gap-1'>
+            <a target='_blank' href={visits[0]?.website.page_url} className='bg-teal-700 py-4 shadow-sm hover:shadow-md hover:cursor-pointer rounded-md text-slate-100 text-center'>Go to website</a>
           </div>
-          <div className='flex-1 bg-white shadow-lg rounded-xl h-full w-full hover:shadow-xl transition-shadow duration-300 p-4 flex flex-col justify-center gap-1'>
-            <span className='text-slate-600'>Visits in past month: <span className='text-indigo-400 font-semibold'>{visitCount}</span></span>
+
+          <div className='flex-1 bg-yellow-100 shadow-lg rounded-xl hover:shadow-xl transition-shadow duration-300 p-4 flex flex-col justify-center gap-1'>
+            <span className='text-slate-600'>Visits in past month: <span className='text-teal-700 font-semibold'>{visitCount}</span></span>
           </div>
         </div>
         </>
