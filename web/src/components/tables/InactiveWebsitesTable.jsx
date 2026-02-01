f71d30186333253ba77f1026999b05cce358fb26
Kārlis <voiconokskarls@gmail.com>
2026-02-01 11:57:35 +0200

    fix overflow problems for inactive websites table

--- web/src/components/tables/InactiveWebsitesTable.jsx
+++ web/src/components/tables/InactiveWebsitesTable.jsx
@@ -21,25 +21,27 @@ const InactiveWebsitesTable = () => {
     <>
     <ContainerTitle title='Inactive Websites' text="See all websites that you haven't visited in over a month." />
 
-    <div className='bg-primary rounded-2xl shadow-lg hover:shadow-xl transition-shadow duration-300 p-8'>
-      <table className='w-full table-fixed text-left border-separate border-spacing-y-2'>
-        <thead>
-          <tr className='shadow-sm rounded-md'>
-            <th className='bg-secondary p-2 rounded-l-md'>Website</th>
-            <th className='bg-secondary p-2 rounded-r-md'>Last Visit</th>
-          </tr>
-        </thead>
-        <tbody>
-          {inactiveWebsites.map(inactiveWebsite => (
-            <tr className='shadow-sm rounded-md' key={inactiveWebsite.host}>
-              <td className='bg-secondary p-2 rounded-l-md'><a target='_blank' href={`https://${inactiveWebsite.host}`}>{inactiveWebsite.host}</a></td>
-              <td className='bg-secondary p-2 rounded-r-md'>
-                <span className='bg-danger text-white py-1 px-4 rounded-2xl'>{inactiveWebsite.last_visit}</span>
-              </td>
+    <div className='bg-primary rounded-2xl shadow-lg hover:shadow-xl transition-shadow duration-300 p-8 h-100 flex justify-center items-center grow'>
+      <div className='h-full w-full overflow-scroll overscroll-contain'>
+        <table className='w-full table-fixed text-left border-separate border-spacing-y-2'>
+          <thead>
+            <tr className='shadow-sm rounded-md'>
+              <th className='bg-secondary p-2 rounded-l-md'>Website</th>
+              <th className='bg-secondary p-2 rounded-r-md'>Last Visit</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {inactiveWebsites.map(inactiveWebsite => (
+              <tr className='shadow-sm rounded-md' key={inactiveWebsite.host}>
+                <td className='bg-secondary p-2 rounded-l-md'><a target='_blank' href={`https://${inactiveWebsite.host}`}>{inactiveWebsite.host}</a></td>
+                <td className='bg-secondary p-2 rounded-r-md'>
+                  <span className='bg-danger text-white py-1 px-4 rounded-2xl'>{inactiveWebsite.last_visit}</span>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
     </>
   )
